3d07041d8f5f3fc52879e105ae8d75545774c9dc
tesar27 <tesar.public@gmail.com>
2026-01-07 15:47:34 +0100

    changing the solution to using Sieve Of Eratosthenes Algorithm

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,18 +1,27 @@
 from sympy import isprime
 
 res = []
-def primeNumbers():
-    global res
-    for i in range(1,250):
-        if i == 1: continue
-        if i == 2 or i == 3: 
-            res.append(i)
-            continue
-        if isprime(i): res.append(i)
-        
-    print(str(res))
 
-primeNumbers()
+def calc_primes(n):
+    if n < 2:
+        return False
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+                
+    primes_only = []
+    for i in range(len(sieve)):
+        if sieve[i] == True:
+            primes_only.append(i)
+
+    return primes_only
+
+res = calc_primes(250)
+
 with open("results.txt", "w", encoding="utf-8") as f:
     f.write("Primer Numbers between 1 and 250\n")
     for i in range(len(res)):
